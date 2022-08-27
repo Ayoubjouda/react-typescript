06ef3579b18b69b4538477c6c0d8210b65747377
Ayoubjouda <ayoubjouda@hotmail.fr>
2022-08-27 11:26:41 +0100

    Redux Setup

---
+++ src/redux/store.js
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { logger } from "redux-logger";
+import rootReducer from "./root-reducer";
+import { persistStore } from "redux-persist";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "./root-sagas";
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [sagaMiddleware];
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
+export const store = configureStore({
+  reducer: rootReducer,
+});
+sagaMiddleware.run(rootSaga);
+export const persistor = persistStore(store);
